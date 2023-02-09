8d9bc33be3b92f6791bdbcd3287e494cf047326d
JoenTNT <tntmasterjoen@gmail.com>
2023-02-09 13:19:10 +0700

    UPDATE - Chapter 4 - Level Packing dengan Scriptable Object

--- Assets/Tutorial Data Driven Game/Scripts/LevelManager.cs
+++ Assets/Tutorial Data Driven Game/Scripts/LevelManager.cs
@@ -2,18 +2,8 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
-    [System.Serializable]
-    private struct DataSoal
-    {
-        public Sprite hint;
-        public string pertanyaan;
-
-        public string[] jawabanTeks;
-        public bool[] adalahBenar;
-    }
-
     [SerializeField]
-    private DataSoal[] _soalSoal = new DataSoal[0];
+    private LevelPackKuis _soalSoal = null;
 
     [SerializeField]
     private UI_Pertanyaan _tempatPertanyaan = null;
@@ -34,13 +24,13 @@ public class LevelManager : MonoBehaviour
         _indexSoal++;
 
         // Jika index melampaui soal terakhir, ulang dari awal
-        if (_indexSoal >= _soalSoal.Length)
+        if (_indexSoal >= _soalSoal.BanyakLevel)
         {
             _indexSoal = 0;
         }
 
         // Ambil data Pertanyaan
-        DataSoal soal = _soalSoal[_indexSoal];
+        LevelSoalKuis soal = _soalSoal.AmbilLevelKe(_indexSoal);
 
         // Set informasi soal
         _tempatPertanyaan.SetPertanyaan($"Level {_indexSoal + 1}", soal.pertanyaan, soal.hint);
@@ -48,7 +38,8 @@ public class LevelManager : MonoBehaviour
         for (int i = 0; i < _tempatPilihanJawaban.Length; i++)
         {
             UI_PoinJawaban poin = _tempatPilihanJawaban[i];
-            poin.SetJawaban(soal.jawabanTeks[i], soal.adalahBenar[i]);
+            LevelSoalKuis.OpsiJawaban opsi = soal.opsiJawaban[i];
+            poin.SetJawaban(opsi.jawabanTeks, opsi.adalahBenar);
         }
     }
 }
---
+++ Assets/Tutorial Data Driven Game/Scripts/LevelSoalKuis.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(
+    fileName = "Soal Baru",
+    menuName = "Game Kuis/Level Soal Kuis")]
+public class LevelSoalKuis : ScriptableObject
+{
+    [System.Serializable]
+    public struct OpsiJawaban
+    {
+        public string jawabanTeks;
+        public bool adalahBenar;
+    }
+
+    public Sprite hint = null;
+    public string pertanyaan = string.Empty;
+
+    public OpsiJawaban[] opsiJawaban = new OpsiJawaban[0];
+}
+
+
+
+
